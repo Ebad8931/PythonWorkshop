b98ee7c95fc9f5434803e22a7e18576f71327aad
Ebad8931 <ebadullah.qureshi@users.noreply.github.com>
2019-08-04 21:37:39 -0400

    Introduction to Python Source Code

---
+++ 01_introduction.py
@@ -0,0 +1,48 @@
+# First Python Program
+print('Hello World')
+
+# Displaying Exercise
+print('He asked,"how are you?"')
+print('The Professor didn’t curve the final exam.')
+print("[FirstName]\n[LastName]")
+print(r"H:\My Drive\Spring 2019\n2e_python_workshop")
+
+# Addition
+print(3 + 4)
+# Subtraction
+print(56 - 34)
+# Multiplication
+print(6 * 7)
+# Exponent
+print(8 ** 2)
+# Division
+print(32 / 8)
+# Floor Division
+print(37 // 8)
+# Remainder
+print(37 % 8)
+
+# String Addition
+print('IEEE ' + 'N2E ' + 'Python Workshop')
+# String Multiplication
+print('python ' * 5)
+
+# Assignment Operators
+var_x = 8
+var_y = 6
+var_y += 4  # y=y+4
+var_x /= 2  # x=x/2
+print(var_x)
+print(var_y)
+
+# Comparison Operators
+print(100 == 100)
+print(110 > 110)
+print(134 >= 134)
+print(143 < 293)
+print(456 <= 345 or "N2E" != "IEEE")
+
+# Logical Operators
+print(True and False)   # Logical AND
+print(True or False)    # Logical OR
+print(not False)        # Logical
